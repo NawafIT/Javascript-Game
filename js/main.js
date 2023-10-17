47ea45f2aefaccbec7c8f5fca7630b56aa4e99e1
NawafIT <nwef918@gmail.com>
2023-10-17 04:10:57 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -14,35 +14,39 @@ if (playGame) {
             let computerChoice = Math.floor(Math.random() * 3 + 1)
 
             let computer =
-             computerChoice === 1 ? "rock" :
-             computerChoice === 2 ? "paper" : "scissors"
+                computerChoice === 1 ? "rock" :
+                    computerChoice === 2 ? "paper" : "scissors"
 
             let result = playerOne === computer ? "Tie Game!"
-               : playerOne === "rock" && computer === "paper"?
-                `palyerOne ${playerOne}\n
+                : playerOne === "rock" && computer === "paper" ?
+                    `palyerOne ${playerOne}\n
                 computer ${computer}\n
-                computer wins!` 
-                : playerOne === "paper" && computer === "scissors"?
-                `palyerOne ${playerOne}\n
+                computer wins!`
+                    : playerOne === "paper" && computer === "scissors" ?
+                        `palyerOne ${playerOne}\n
                 computer ${computer}\n
-                computer wins!` 
-                : playerOne === "scissors" && computer === "scissors"?
-                `palyerOne ${playerOne}\n
+                computer wins!`
+                        : playerOne === "scissors" && computer === "rock" ?
+                            `palyerOne ${playerOne}\n
                 computer ${computer}\n
-                computer wins!` 
-                :`palyerOne ${playerOne}\n
+                computer wins!`
+                            : `palyerOne ${playerOne}\n
                 computer ${computer}\n
                 palyerOne wins!`
-                
+
             alert(result)
             let playAgin = confirm("Play again?")
-            playAgin? location.reload() : alert("Ok thanks for playing")  
+            playAgin ? location.reload() : alert("Ok thanks for playing")
         }
         else {
             let tryAgain = confirm("Your input is wrong!, do you want to try again?")
-            tryAgain? location.reload() : alert("Ok, maybe next time")
+            tryAgain ? location.reload() : alert("Ok, maybe next time")
         }
 
+    } else if (playerChoice === "") {
+
+        let tryAgain = confirm("To play you must enter rock, paper, or scissors, do you want to try again?")
+        tryAgain ? location.reload() : alert("Ok, maybe next time")
     } else {
         alert("I guess you chanaged you mind, maybe next time!")
     }
